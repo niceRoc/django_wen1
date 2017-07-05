4074b04b65dd84dbbb7f0f170da9610816c25caa
niceRoc <1016579757@qq.com>
2017-07-05 11:54:10 +0800

    初始化用户模块中注册，订单，收货地址，个人中心的视图，让所有的模板页初始化显示出来

--- django_web1/app_user/urls.py
+++ django_web1/app_user/urls.py
@@ -9,4 +9,8 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.login),  # 用户登录页面url
+    url(r'^register/$', views.register),  # 用户注册页面
+    url(r'^info/$', views.user_center_info),  # 用户中心--个人中心页面
+    url(r'^order/$', views.user_center_order),  # 用户中心--全部订单页面
+    url(r'^site/$', views.user_center_site),  # 用户中心--收货地址页面
 ]
--- django_web1/app_user/views.py
+++ django_web1/app_user/views.py
@@ -10,3 +10,27 @@ def login(request):
     """用户登录页面"""
 
     return render(request, 'app_user/login.html')
+
+
+def register(request):
+    """用户注册页面"""
+
+    return render(request, 'app_user/register.html')
+
+
+def user_center_info(request):
+    """用户中心 -- 个人信息页面"""
+
+    return render(request, 'app_user/user_center_info.html')
+
+
+def user_center_order(request):
+    """用户中心 -- 全部订单页面"""
+
+    return render(request, 'app_user/user_center_order.html')
+
+
+def user_center_site(request):
+    """用户中心 -- 收货地址页面"""
+
+    return render(request, 'app_user/user_center_site.html')
