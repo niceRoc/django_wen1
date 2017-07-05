3effb8597f721c7492a846f14b119f6e9f61c1e2
niceRoc <1016579757@qq.com>
2017-07-05 13:33:04 +0800

    完成用户模块的模板继承功能

--- django_web1/app_user/views.py
+++ django_web1/app_user/views.py
@@ -9,28 +9,29 @@ from django.shortcuts import render
 def login(request):
     """用户登录页面"""
 
-    return render(request, 'app_user/login.html')
+    return render(request, 'app_user/login.html', {'title': '天天生鲜-登录'})
 
 
 def register(request):
     """用户注册页面"""
 
-    return render(request, 'app_user/register.html')
+    return render(request, 'app_user/register.html', {'title': '天天生鲜-注册'})
 
 
 def user_center_info(request):
     """用户中心 -- 个人信息页面"""
 
-    return render(request, 'app_user/user_center_info.html')
+    return render(request, 'app_user/user_center_info.html', {'title': '天天生鲜-用户中心'})
 
 
 def user_center_order(request):
     """用户中心 -- 全部订单页面"""
 
-    return render(request, 'app_user/user_center_order.html')
+    return render(request, 'app_user/user_center_order.html', {'title': '天天生鲜-用户中心'})
 
 
 def user_center_site(request):
     """用户中心 -- 收货地址页面"""
 
-    return render(request, 'app_user/user_center_site.html')
+    return render(request, 'app_user/user_center_site.html', {'title': '天天生鲜-用户中心'})
+
